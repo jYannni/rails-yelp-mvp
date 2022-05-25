9abbd62ff750ce394eec927f371f46a9d61ab373
Yanko Thomson <thomsonyani@gmail.com>
2022-05-25 14:13:12 +0800

    Fix: Created the model, route, controller and view for index, created the model for Review

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/restaurants', to: 'restaurants#index'
+  # get '/restaurants/new', to: 'restaurants#new'
+  # post '/restaurants', to: 'restaurants#create'
+  # get '/restaurants/:id' to: restaurants#show
+  # get	'/photos/:id/edit'	to: 'photos#edit'
+  # patch	'/photos/:id'	to: 'photos#update'
+  # delete	'/photos/:id'	to: 'photos#destroy'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts 'Destroying data'
+Restaurant.destroy_all
+puts 'Starting seeds'
+10.times do
+  Restaurant.create(name: Faker::Restaurant.name, address: Faker::Address.city, category: Restaurant::CATEGORY.sample)
+end
+puts "#{Restaurant.count} created"
+puts restaurants
